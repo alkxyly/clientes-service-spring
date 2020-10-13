5587908642e1316f30369d708180453cdc7573f8
alkxyly <alkxyly@hotmail.com>
2020-10-13 00:36:22 -0300

    Cadastro de Servicos Prestados

--- src/main/java/io/github/alkxyly/clientes/rest/ServicoPrestadoController.java
+++ src/main/java/io/github/alkxyly/clientes/rest/ServicoPrestadoController.java
@@ -1,34 +1,30 @@
 package io.github.alkxyly.clientes.rest;
 
-import com.sun.org.apache.xerces.internal.impl.dv.xs.DateTimeDV;
 import io.github.alkxyly.clientes.model.entity.Cliente;
 import io.github.alkxyly.clientes.model.entity.ServicoPrestado;
 import io.github.alkxyly.clientes.model.repository.ClienteRepository;
 import io.github.alkxyly.clientes.model.repository.ServicoPrestadoRepository;
 import io.github.alkxyly.clientes.rest.dto.ServicoPrestadoDTO;
+import io.github.alkxyly.clientes.util.BigDecimalConverter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/servicos-prestados")
 @RequiredArgsConstructor
 public class ServicoPrestadoController {
 
-    private  final ClienteRepository clienteRepository;
+    private final ClienteRepository clienteRepository;
     private final ServicoPrestadoRepository servicoPrestadoRepository;
+    private final BigDecimalConverter bigDecimalConverter;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto){
         ServicoPrestado servicoPrestado = new ServicoPrestado();
         Integer idCliente = dto.getIdCliente();
@@ -39,7 +35,7 @@ public class ServicoPrestadoController {
         servicoPrestado.setDescricao(dto.getDescricao());
         servicoPrestado.setData(LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
         servicoPrestado.setCliente(cliente);
-        servicoPrestado.setValor(new BigDecimal(dto.getPreco()));
-        return servicoPrestado;
+        servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
+        return servicoPrestadoRepository.save(servicoPrestado);
     }
 }
